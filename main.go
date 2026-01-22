6439d038dd9be295ca817699016ed9986d9feb7d
maruuanh <marwanhadid6@gmail.com>
2026-01-22 23:02:37 +0700

    feat: redirect to swagger page when application started

--- main.go
+++ main.go
@@ -11,13 +11,27 @@ import (
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// @title Kasir API
 	// @version 1.0
 	// @description API sederhana untuk manajemen produk di kasir
 	// @host localhost:8080
-	// @BasePath /
-	// @schemes http
+	// @schemes http https
 
 	var produk = models.DataProduk
 
@@ -49,7 +63,7 @@ func main() {
 	fmt.Println("Server running di localhost:8080")
 
 	// Jalankan server di port 8080
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", enableCORS(http.NewServeMux()))
 
 	// Tangani error jika server gagal dijalankan
 	if err != nil {
