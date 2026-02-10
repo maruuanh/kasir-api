791882524394da4405393682be924b9373846dc9
maruuanh <marwanhadid6@gmail.com>
2026-02-10 21:22:54 +0700

    fix bug on cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -6,7 +6,7 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-API-Key")
+		w.Header().Set("Access-Control-Allow-Header", "X-Api-Key")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
